0f2ebce06f938626bd2423d8a7fe42d1aae51926
jghuf <33262373+jghuf@users.noreply.github.com>
2017-12-31 18:53:40 +0100

    Add files via upload

---
+++ EulerProblem2/EulerProblem2.py
@@ -0,0 +1,28 @@
+'''
+Created on Dec 31, 2017
+
+@author: jghuf
+'''
+import time 
+
+def sumOfEvenFibonacci(limit):
+    fib0 = 1
+    fib1 = 1
+    fib2 = 0
+    sum = 0
+    while fib2 < limit:
+        fib2 = fib0 + fib1
+        if (fib2 % 2) == 0:
+            sum = sum + fib2
+        fib0 = fib1
+        fib1 = fib2
+    return sum
+
+
+if __name__ == '__main__':
+    limit = 4000000
+    start = time.time()
+    sumofEven =  sumOfEvenFibonacci(limit)
+    elapsed = (time.time() - start)
+ 
+    print("Sum of even Fibonacci numbers up to "+ str(limit)+" is %s , found in %s seconds" % (sumofEven,elapsed))
